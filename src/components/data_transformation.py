75082ba7d7b9b7e7ec69838db5805724418f5be3
AlyssonRodriguez05 <alyssonrodriguez03@gmail.com>
2024-02-25 21:56:13 +0100

    Update data_transformation.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -6,18 +6,11 @@ from src.utils import preprocess_data
 
 import pandas as pd
 import numpy as np
-<<<<<<< HEAD
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-=======
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
 from src.utils import save_object
 
->>>>>>> main
 from dataclasses import dataclass
 
 
@@ -36,49 +29,6 @@ class DataTransformation:
         columns_to_drop = ['Order', 'PID']
         X = df.drop(columns_to_drop, axis=1)
         
-<<<<<<< HEAD
-        # Define the numerical and categorical columns
-        numerical_cols = X.select_dtypes(include=['int64', 'float64']).columns
-        categorical_cols = X.select_dtypes(include=['object']).columns
-
-        # Define the numerical and categorical transformers
-        numerical_transformer = Pipeline(steps=[
-            ('imputer', SimpleImputer(strategy='median')),
-            ('scaler', StandardScaler())
-        ])
-        
-        categorical_transformer = Pipeline(steps=[
-            ('imputer', SimpleImputer(strategy='constant', fill_value='None')),
-            ('onehot', OneHotEncoder(handle_unknown='ignore', sparse=False))
-        ])
-
-        # Combine transformers into a ColumnTransformer
-        preprocessor = ColumnTransformer(
-            transformers=[
-                ('num', numerical_transformer, numerical_cols),
-                ('cat', categorical_transformer, categorical_cols)
-            ])
-        
-        # Apply the preprocessing pipeline to the dataset
-        X_processed = preprocessor.fit_transform(X)
-
-        # The result is a numpy array, so we should convert it to a DataFrame
-        # Get the list of one-hot encoded column names from the pipeline
-        ohe_columns = preprocessor.named_transformers_['cat'].named_steps['onehot'].get_feature_names_out(categorical_cols)
-
-        # Combine numerical and one-hot encoded column names
-        processed_columns = numerical_cols.tolist() + ohe_columns.tolist()
-
-        # Create the processed DataFrame
-        X_processed_df = pd.DataFrame(X_processed, columns=processed_columns)
-
-        # Return the processed DataFrame
-        return X_processed_df
-
-
-
-
-=======
     def transform_data(self, raw_path):
         '''
         This function is responsible for data transformation
@@ -114,6 +64,5 @@ class DataTransformation:
             
         except Exception as e:
             raise CustomException(e,sys)
->>>>>>> main
         
      
